3582dd2b0e5536a10462993f286754ab6be2e746
Aaron Strouse <aaronstrouse@gmail.com>
2017-03-25 20:32:16 -0400

    intro to JSX replacing createElement and need for Babel

--- dist/index.js
+++ dist/index.js
@@ -1,20 +1,19 @@
-const {createElement} = React
+
 const { render } = ReactDOM
 
-const style = {
-    backgroundColor: 'orange',
-    color: 'white',
-    fontFamily: 'verdana'
-}
 
-const title = createElement(
-    'h1',
-    {id: 'title', className: 'header',style: style},
-    'Hello World'
-)
+//JSX is JavaScript as XML, which is tag-based syntax used to create React elements.
+//JSX replaces need for createElement calls. So createElement is removed at top of file. 
+// JSX expressions are wrapped in curly brackets. 
+
 
   
-render(
-    title,
+render( //w/out Babel, tag on line 10 throws an error of "unexpected token"
+    <h1 id="title"
+        classname='header'
+        style={{backgroundColor: 'orange', color: 'white', fontFamily: 'verdana'}}>
+        Hello World
+    </h1>,
+  
     document.getElementById('react-container')
 )
