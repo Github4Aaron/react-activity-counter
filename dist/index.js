b1d8c1bacf22c4a6e3c915fdfdf93ec3533dd373
Aaron Strouse <aaronstrouse@gmail.com>
2017-03-25 20:17:51 -0400

    ES6 destructuring, created style element

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,20 @@
-const title = React.createElement(
+const {createElement} = React
+const { render } = ReactDOM
+
+const style = {
+    backgroundColor: 'orange',
+    color: 'white',
+    fontFamily: 'verdana'
+}
+
+const title = createElement(
     'h1',
-    {id: 'title', className: 'header'},
+    {id: 'title', className: 'header',style: style},
     'Hello World'
 )
 
-ReactDOM.render(
+  
+render(
     title,
     document.getElementById('react-container')
 )
