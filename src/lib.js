5e20c0ff53094e35f2d705f116d199a8efa7ebd9
Aaron Strouse <aaronstrouse@gmail.com>
2017-03-26 20:21:38 -0400

    using webpack to transpile SCSS to CSS

--- src/lib.js
+++ src/lib.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import text from './titles.json'
+import './stylesheets/goodbye.scss'
+import './stylesheets/hello.css'
 
 export const hello = (
     <h1 id='title' 
-           className='header' 
-           style={{backgroundColor: 'purple', color: 'yellow'}}>
+           className='hello'> 
            {text.hello}
     </h1>
 )
 
 export const goodbye = (
     <h1 id='title'
-        className='header'
-        style={{backgroundColor: 'yellow', color: 'purple'}}>
+        className='goodbye'>
         {text.goodbye}
     </h1>
 )
+
+//webpack is bundling CSS so we don't have to make additional HTTP requests
+//Webpack is providing Babel, transpiling SCSS to CSS, and loading JSON.
