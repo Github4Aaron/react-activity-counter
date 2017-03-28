4837fc5846ce20cb8fed42f8874788056d76e105
Aaron Strouse <aaronstrouse@gmail.com>
2017-03-28 14:49:29 -0400

    added default props in a createClass-type component

--- src/index.js
+++ src/index.js
@@ -1,40 +1,43 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { SkiDayCount } from './components/SkiDayCount'
-import { SkiDayList } from './components/SkiDayList'
+import { SkiDayCount1 } from './components/SkiDayCount-createClass.js'
+
 
 window.React = React
 
 
-
-
 render(
-	<SkiDayList days={
-		[
-			{ // the objects in this array will populate the rows in the table
-				resort: "Squaw Valley",
-				date: new Date ("1/2/2016"),
-				powder: true,
-				backCountry: false
-			}, 
-			{ 
-				resort: "Kirkwood",
-				date: new Date ("3/28/2016"),
-				powder: false,
-				backCountry: false
-			},
-			{ 
-				resort: "Mt Tallac",
-				date: new Date ("4/2/2016"),
-				powder: true,
-				backCountry: false
-			},
-			
-		]
-
-	}/>,
+	<SkiDayCount1 />,
 	document.getElementById('react-container')
 )
+
+// render(
+// 	<SkiDayList days={
+// 		[
+// 			{ // the objects in this array will populate the rows in the table
+// 				resort: "Squaw Valley",
+// 				date: new Date ("1/2/2016"),
+// 				powder: true,
+// 				backCountry: false
+// 			}, 
+// 			{ 
+// 				resort: "Kirkwood",
+// 				date: new Date ("3/28/2016"),
+// 				powder: false,
+// 				backCountry: false
+// 			},
+// 			{ 
+// 				resort: "Mt Tallac",
+// 				date: new Date ("4/2/2016"),
+// 				powder: true,
+// 				backCountry: false
+// 			},
+			
+// 		]
+
+// 	}/>,
+// 	document.getElementById('react-container')
+// )
 
 //properties are an object and every property is a key. 
 //Dynamic data supplied by this.props
