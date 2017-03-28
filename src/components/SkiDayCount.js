ded37de0316d99ee5ab79f9436a54a26d5900cfe
Aaron Strouse <aaronstrouse@gmail.com>
2017-03-27 23:11:16 -0400

    created stateless functional component, removed this, and passed in props

--- src/components/SkiDayCount.js
+++ src/components/SkiDayCount.js
@@ -1,44 +1,39 @@
 import { Component } from 'react'
 import '../stylesheets/ui.scss'
 
-export class SkiDayCount extends Component {
-	percentToDecimal(decimal) {  
-		return((decimal * 100) + '%')
-	}
-	calcGoalProgress(total, goal) {
-		return this.percentToDecimal(total/goal)
-	}
-	
-	render() {
-		return (
-			<div className="ski-day-count">
-				<div className="total-days">
-					<span>{this.props.total}</span>
-					<span>days</span>
-				</div>
-				<div className="powder-days">
-					<span>{this.props.powder}</span>
-					<span>days</span>
-				</div>
-				<div className="backcountry-days">
-					<span>{this.props.backcountry}</span>
-					<span>hiking day</span>
-				</div>
-				<div>
-					<span>{this.calcGoalProgress( 
-							this.props.total,
-							this.props.goal)}</span>
-				</div>
-			</div>
-		)
-	}
+const percentToDecimal = (decimal) => {
+	return((decimal * 100) + '%')
 }
 
-// ES6, released in 2015, describes how JS should be implemented by browsers.
-//Class syntax is one feature of ES6. 
-//Differences with ES6 Class Syntax:
-		//"Class" and "extends" replace "createClass"
-		//Methods do not need commas after them.
-		//Ability to import specific part  of React ("extends Component")
-		//Data binding must be done manually
+const calcGoalProgress = (total, goal) => {
+		return percentToDecimal(total/goal)
+	}
+
+export const SkiDayCount = (props) => (
+		<div className="ski-day-count">
+			<div className="total-days">
+				<span>{props.total}</span>
+				<span>days</span>
+			</div>
+			<div className="powder-days">
+				<span>{props.powder}</span>
+				<span>days</span>
+			</div>
+			<div className="backcountry-days">
+				<span>{props.backcountry}</span>
+				<span>hiking day</span>
+			</div>
+			<div>
+				<span>{calcGoalProgress( 
+						props.total,
+						props.goal)}</span>
+			</div>
+		</div>
+	)
+	
+// 3 ways to make component: (1) createClass, (2) ES6 / class extends, (3) stateless functions component.
+//Stateless functions do NOT have access to "this", so properties are passed directly into the function. 
+//Local methods must be moved and put into their own functions. 
+
+
 
