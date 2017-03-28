b47e7b8857f4342bcdef042107583bfc3a437106
Aaron Strouse <aaronstrouse@gmail.com>
2017-03-27 22:54:32 -0400

    ES6 destructuring, used Class & Extends languge to replace createClass

--- src/components/SkiDayCount.js
+++ src/components/SkiDayCount.js
@@ -1,13 +1,13 @@
-import React from 'react'
+import { Component } from 'react'
 import '../stylesheets/ui.scss'
 
-export const SkiDayCount = React.createClass({
-	percentToDecimal(decimal) {  //these methods are local to this component
+export class SkiDayCount extends Component {
+	percentToDecimal(decimal) {  
 		return((decimal * 100) + '%')
-	},
+	}
 	calcGoalProgress(total, goal) {
 		return this.percentToDecimal(total/goal)
-	},
+	}
 	
 	render() {
 		return (
@@ -25,14 +25,20 @@ export const SkiDayCount = React.createClass({
 					<span>hiking day</span>
 				</div>
 				<div>
-					<span>{this.calcGoalProgress( //props are passed in as arguments to function
+					<span>{this.calcGoalProgress( 
 							this.props.total,
 							this.props.goal)}</span>
 				</div>
 			</div>
 		)
 	}
-})
+}
 
-//Instead of hard-coded data, using dynamic data supplied from props.
+// ES6, released in 2015, describes how JS should be implemented by browsers.
+//Class syntax is one feature of ES6. 
+//Differences with ES6 Class Syntax:
+		//"Class" and "extends" replace "createClass"
+		//Methods do not need commas after them.
+		//Ability to import specific part  of React ("extends Component")
+		//Data binding must be done manually
 
