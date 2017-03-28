46c2975d13974f3e9d29023837ac607c24fdb8b5
Aaron Strouse <aaronstrouse@gmail.com>
2017-03-28 13:40:09 -0400

    displaying a child component & using JS map function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { SkiDayCount } from './components/SkiDayCount'
+import { SkiDayList } from './components/SkiDayList'
 
 window.React = React
 
@@ -8,12 +9,35 @@ window.React = React
 
 
 render(
-	<SkiDayCount total={50} // numbers are wrapped in JSX expressions
-				 powder={12}
-				 backcountry={10}
-				 goal={100}/>,
+	<SkiDayList days={
+		[
+			{ // the objects in this array will populate the rows in the table
+				resort: "Squaw Valley",
+				date: new Date ("1/2/2016"),
+				powder: true,
+				backCountry: false
+			}, 
+			{ 
+				resort: "Kirkwood",
+				date: new Date ("3/28/2016"),
+				powder: false,
+				backCountry: false
+			},
+			{ 
+				resort: "Mt Tallac",
+				date: new Date ("4/2/2016"),
+				powder: true,
+				backCountry: false
+			},
+			
+		]
+
+	}/>,
 	document.getElementById('react-container')
 )
 
 //properties are an object and every property is a key. 
 //Dynamic data supplied by this.props
+
+// SkiDayList data will render inside SkiDayList component. 
+//SkiDayRow is the child component to SkiDayList. 
